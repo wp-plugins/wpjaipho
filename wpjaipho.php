04214ecfa17dfda73d5bdcb9c53b8da9b94eae34
tole.car <tole.car@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-05 20:05:16 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpjaipho/tags/1.2.0@595108 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpjaipho.php
+++ wpjaipho.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: WPJaipho
-Version: 1.1.1
+Version: 1.2.0
 Plugin URI: http://www.jaipho.com
 Description: WPJaipho is a mobile image gallery plugin optimized for iPhone and iPad users. 
 Author: tole
